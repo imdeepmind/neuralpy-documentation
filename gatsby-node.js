303b30c3dcaddaf7545030dc1dc57c7d1ba7fd95
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-07-19 23:35:41 +0530

    added code for generating dynamic pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,15 +1,44 @@
 const path = require("path")
 
 exports.onCreateNode = ({ node, actions }) => {
-    const { createNodeField } = actions
+  const { createNodeField } = actions
 
-    if (node.internal.type === "MarkdownRemark") {
-        const slug = path.basename(node.fileAbsolutePath, '.md')
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = path.basename(node.fileAbsolutePath, ".md")
 
-        createNodeField({
-            node,
-            name: 'slug',
-            value: slug
-        })
-    }
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug,
+    })
   }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(`src/pages/docs.js`)
+
+  const result = await graphql(`
+    query loadPagesQuery {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      path: `/docs/${edge.node.fields.slug}`,
+      component: blogPostTemplate,
+      context: {
+        slug: edge.node.fields.slug,
+      },
+    })
+  })
+}
